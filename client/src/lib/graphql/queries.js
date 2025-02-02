168f21474047d8cba82705d806d1da5b43beeb38
Furkan Gündüz <furkan.gundz00@gmail.com>
2025-02-02 21:39:05 +0300

    implement job detail retrieval and update JobPage component to display job information

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -20,3 +20,22 @@ export async function getJobs() {
   const { jobs } = await client.request(query);
   return jobs;
 }
+
+export async function getJob(id) {
+  const query = gql`
+    query ($id: ID!) {
+      job(id: $id) {
+        id
+        title
+        description
+        date
+        company {
+          id
+        }
+      }
+    }
+  `;
+
+  const { job } = await client.request(query, { id });
+  return job;
+}
--- client/src/pages/JobPage.js
+++ client/src/pages/JobPage.js
@@ -1,31 +1,34 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { formatDate } from '../lib/formatters';
-import { jobs } from '../lib/fake-data';
+import { getJob } from '../lib/graphql/queries';
 
 function JobPage() {
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
   const { jobId } = useParams();
 
-  const job = jobs.find((job) => job.id === jobId);
+  console.log(job);
+  useEffect(() => {
+    getJob(jobId)
+      .then((job) => setJob(job))
+      .finally(() => setLoading(false));
+  }, [jobId]);
+
   return (
-    <div>
-      <h1 className="title is-2">
-        {job.title}
-      </h1>
-      <h2 className="subtitle is-4">
-        <Link to={`/companies/${job.company.id}`}>
-          {job.company.name}
-        </Link>
-      </h2>
-      <div className="box">
-        <div className="block has-text-grey">
-          Posted: {formatDate(job.date, 'long')}
+    !loading && (
+      <div>
+        <h1 className='title is-2'>{job.title}</h1>
+        <h2 className='subtitle is-4'>
+          <Link to={`/companies/${job.company.id}`}>{job.company.name}</Link>
+        </h2>
+        <div className='box'>
+          <div className='block has-text-grey'>Posted: {formatDate(job.date, 'long')}</div>
+          <p className='block'>{job.description}</p>
         </div>
-        <p className="block">
-          {job.description}
-        </p>
       </div>
-    </div>
+    )
   );
 }
 
